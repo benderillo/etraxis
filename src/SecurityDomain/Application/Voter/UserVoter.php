74f760e0d0633252a7045b0fb1c7ae9f505efd5f
Artem Rodygin <artem.rodygin@gmail.com>
2018-04-08 14:00:09 +1200

    User voter

---
+++ src/SecurityDomain/Application/Voter/UserVoter.php
@@ -0,0 +1,188 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\SecurityDomain\Application\Voter;
+
+use eTraxis\SecurityDomain\Model\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+/**
+ * Voter for "User" entities.
+ */
+class UserVoter extends Voter
+{
+    public const CREATE_USER  = 'user.create';
+    public const UPDATE_USER  = 'user.update';
+    public const DELETE_USER  = 'user.delete';
+    public const DISABLE_USER = 'user.disable';
+    public const ENABLE_USER  = 'user.enable';
+    public const UNLOCK_USER  = 'user.unlock';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        $attributes = [
+            self::CREATE_USER  => null,
+            self::UPDATE_USER  => User::class,
+            self::DELETE_USER  => User::class,
+            self::DISABLE_USER => User::class,
+            self::ENABLE_USER  => User::class,
+            self::UNLOCK_USER  => User::class,
+        ];
+
+        // Whether the attribute is supported.
+        if (!array_key_exists($attribute, $attributes)) {
+            return false;
+        }
+
+        // Whether the subject is not required.
+        if ($attributes[$attribute] === null) {
+            return true;
+        }
+
+        // The subject must be an object of expected class.
+        return is_object($subject) && get_class($subject) === $attributes[$attribute];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @codeCoverageIgnore
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        // User must be logged in.
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        /** @var User $subject */
+        switch ($attribute) {
+
+            case self::CREATE_USER:
+                return $this->isCreateGranted($user);
+
+            case self::UPDATE_USER:
+                return $this->isUpdateGranted($subject, $user);
+
+            case self::DELETE_USER:
+                return $this->isDeleteGranted($subject, $user);
+
+            case self::DISABLE_USER:
+                return $this->isDisableGranted($subject, $user);
+
+            case self::ENABLE_USER:
+                return $this->isEnableGranted($subject, $user);
+
+            case self::UNLOCK_USER:
+                return $this->isUnlockGranted($subject, $user);
+
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Whether the current user can create a new one.
+     *
+     * @param User $user Current user.
+     *
+     * @return bool
+     */
+    protected function isCreateGranted(User $user): bool
+    {
+        return $user->isAdmin;
+    }
+
+    /**
+     * Whether the specified user can be updated.
+     *
+     * @param User $subject Subject user.
+     * @param User $user    Current user.
+     *
+     * @return bool
+     */
+    protected function isUpdateGranted(User $subject, User $user): bool
+    {
+        return $user->isAdmin || $subject->id === $user->id;
+    }
+
+    /**
+     * Whether the specified user can be deleted.
+     *
+     * @param User $subject Subject user.
+     * @param User $user    Current user.
+     *
+     * @return bool
+     */
+    protected function isDeleteGranted(User $subject, User $user): bool
+    {
+        // Can't delete oneself.
+        if ($subject->id === $user->id) {
+            return false;
+        }
+
+        /** @todo Can't delete user if mentioned in an issue history. */
+
+        return $user->isAdmin;
+    }
+
+    /**
+     * Whether the specified user can be disabled.
+     *
+     * @param User $subject Subject user.
+     * @param User $user    Current user.
+     *
+     * @return bool
+     */
+    protected function isDisableGranted(User $subject, User $user): bool
+    {
+        // Can't disable oneself.
+        if ($subject->id === $user->id) {
+            return false;
+        }
+
+        return $user->isAdmin;
+    }
+
+    /**
+     * Whether the specified user can be enabled.
+     *
+     * @param User $subject Subject user.
+     * @param User $user    Current user.
+     *
+     * @return bool
+     */
+    protected function isEnableGranted(User $subject, User $user): bool
+    {
+        return $user->isAdmin;
+    }
+
+    /**
+     * Whether the specified user can be unlocked.
+     *
+     * @param User $subject Subject user.
+     * @param User $user    Current user.
+     *
+     * @return bool
+     */
+    protected function isUnlockGranted(User $subject, User $user): bool
+    {
+        return $user->isAdmin;
+    }
+}
---
+++ tests/SecurityDomain/Application/Voter/UserVoterTest.php
@@ -0,0 +1,159 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\SecurityDomain\Application\Voter;
+
+use eTraxis\SecurityDomain\Model\Entity\User;
+use eTraxis\Tests\ReflectionTrait;
+use eTraxis\Tests\TransactionalTestCase;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+
+class UserVoterTest extends TransactionalTestCase
+{
+    use ReflectionTrait;
+
+    /** @var \Symfony\Component\Security\Core\Authorization\AuthorizationChecker */
+    protected $security;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->security = $this->client->getContainer()->get('security.authorization_checker');
+    }
+
+    public function testUnsupportedAttribute()
+    {
+        /** @var \eTraxis\SecurityDomain\Model\Repository\UserRepository $repository */
+        $repository = $this->doctrine->getRepository(User::class);
+
+        $nhills = $repository->findOneByUsername('nhills@example.com');
+
+        $this->loginAs('admin@example.com');
+        self::assertFalse($this->security->isGranted('UNKNOWN', $nhills));
+    }
+
+    public function testAnonymous()
+    {
+        $voter = new UserVoter();
+        $token = new AnonymousToken('', 'anon.');
+
+        /** @var \eTraxis\SecurityDomain\Model\Repository\UserRepository $repository */
+        $repository = $this->doctrine->getRepository(User::class);
+
+        $nhills = $repository->findOneByUsername('nhills@example.com');
+
+        self::assertSame(UserVoter::ACCESS_DENIED, $voter->vote($token, $nhills, [UserVoter::CREATE_USER]));
+        self::assertSame(UserVoter::ACCESS_DENIED, $voter->vote($token, $nhills, [UserVoter::UPDATE_USER]));
+        self::assertSame(UserVoter::ACCESS_DENIED, $voter->vote($token, $nhills, [UserVoter::DELETE_USER]));
+        self::assertSame(UserVoter::ACCESS_DENIED, $voter->vote($token, $nhills, [UserVoter::DISABLE_USER]));
+        self::assertSame(UserVoter::ACCESS_DENIED, $voter->vote($token, $nhills, [UserVoter::ENABLE_USER]));
+        self::assertSame(UserVoter::ACCESS_DENIED, $voter->vote($token, $nhills, [UserVoter::UNLOCK_USER]));
+    }
+
+    public function testCreate()
+    {
+        $this->loginAs('admin@example.com');
+        self::assertTrue($this->security->isGranted(UserVoter::CREATE_USER));
+
+        $this->loginAs('artem@example.com');
+        self::assertFalse($this->security->isGranted(UserVoter::CREATE_USER));
+    }
+
+    public function testUpdate()
+    {
+        /** @var \eTraxis\SecurityDomain\Model\Repository\UserRepository $repository */
+        $repository = $this->doctrine->getRepository(User::class);
+
+        $nhills = $repository->findOneByUsername('nhills@example.com');
+        $artem  = $repository->findOneByUsername('artem@example.com');
+
+        $this->loginAs('admin@example.com');
+        self::assertTrue($this->security->isGranted(UserVoter::UPDATE_USER, $nhills));
+
+        $this->loginAs('artem@example.com');
+        self::assertFalse($this->security->isGranted(UserVoter::UPDATE_USER, $nhills));
+        self::assertTrue($this->security->isGranted(UserVoter::UPDATE_USER, $artem));
+    }
+
+    public function testDelete()
+    {
+        /** @var \eTraxis\SecurityDomain\Model\Repository\UserRepository $repository */
+        $repository = $this->doctrine->getRepository(User::class);
+
+        $nhills = $repository->findOneByUsername('nhills@example.com');
+        $admin  = $repository->findOneByUsername('admin@example.com');
+
+        $this->loginAs('admin@example.com');
+        self::assertTrue($this->security->isGranted(UserVoter::DELETE_USER, $nhills));
+        self::assertFalse($this->security->isGranted(UserVoter::DELETE_USER, $admin));
+
+        $this->loginAs('artem@example.com');
+        self::assertFalse($this->security->isGranted(UserVoter::DELETE_USER, $nhills));
+    }
+
+    public function testDisable()
+    {
+        /** @var \eTraxis\SecurityDomain\Model\Repository\UserRepository $repository */
+        $repository = $this->doctrine->getRepository(User::class);
+
+        $nhills = $repository->findOneByUsername('nhills@example.com');
+        $tberge = $repository->findOneByUsername('tberge@example.com');
+        $admin  = $repository->findOneByUsername('admin@example.com');
+
+        $this->loginAs('admin@example.com');
+        self::assertTrue($this->security->isGranted(UserVoter::DISABLE_USER, $nhills));
+        self::assertTrue($this->security->isGranted(UserVoter::DISABLE_USER, $tberge));
+        self::assertFalse($this->security->isGranted(UserVoter::DISABLE_USER, $admin));
+
+        $this->loginAs('artem@example.com');
+        self::assertFalse($this->security->isGranted(UserVoter::DISABLE_USER, $nhills));
+        self::assertFalse($this->security->isGranted(UserVoter::DISABLE_USER, $tberge));
+    }
+
+    public function testEnable()
+    {
+        /** @var \eTraxis\SecurityDomain\Model\Repository\UserRepository $repository */
+        $repository = $this->doctrine->getRepository(User::class);
+
+        $nhills = $repository->findOneByUsername('nhills@example.com');
+        $tberge = $repository->findOneByUsername('tberge@example.com');
+        $admin  = $repository->findOneByUsername('admin@example.com');
+
+        $this->loginAs('admin@example.com');
+        self::assertTrue($this->security->isGranted(UserVoter::ENABLE_USER, $nhills));
+        self::assertTrue($this->security->isGranted(UserVoter::ENABLE_USER, $tberge));
+        self::assertTrue($this->security->isGranted(UserVoter::ENABLE_USER, $admin));
+
+        $this->loginAs('artem@example.com');
+        self::assertFalse($this->security->isGranted(UserVoter::ENABLE_USER, $nhills));
+        self::assertFalse($this->security->isGranted(UserVoter::ENABLE_USER, $tberge));
+    }
+
+    public function testUnlock()
+    {
+        /** @var \eTraxis\SecurityDomain\Model\Repository\UserRepository $repository */
+        $repository = $this->doctrine->getRepository(User::class);
+
+        $nhills = $repository->findOneByUsername('nhills@example.com');
+        $zapp   = $repository->findOneByUsername('jgutmann@example.com');
+
+        $this->loginAs('admin@example.com');
+        self::assertTrue($this->security->isGranted(UserVoter::UNLOCK_USER, $nhills));
+        self::assertTrue($this->security->isGranted(UserVoter::UNLOCK_USER, $zapp));
+
+        $this->loginAs('artem@example.com');
+        self::assertFalse($this->security->isGranted(UserVoter::UNLOCK_USER, $nhills));
+        self::assertFalse($this->security->isGranted(UserVoter::UNLOCK_USER, $zapp));
+    }
+}
