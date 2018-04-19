8b23948b972cd0917314a6e309ed6c9e91aa4b8b
Artem Rodygin <artem.rodygin@gmail.com>
2018-04-19 20:49:18 +1200

    SetFieldPositionCommand

---
+++ src/TemplatesDomain/Application/CommandHandler/Fields/SetFieldPositionHandler.php
@@ -0,0 +1,116 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\TemplatesDomain\Application\CommandHandler\Fields;
+
+use Doctrine\ORM\EntityManagerInterface;
+use eTraxis\TemplatesDomain\Application\Command\Fields\SetFieldPositionCommand;
+use eTraxis\TemplatesDomain\Application\Voter\FieldVoter;
+use eTraxis\TemplatesDomain\Model\Entity\Field;
+use eTraxis\TemplatesDomain\Model\Repository\FieldRepository;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+/**
+ * Command handler.
+ */
+class SetFieldPositionHandler
+{
+    protected $security;
+    protected $repository;
+    protected $manager;
+
+    /**
+     * Dependency Injection constructor.
+     *
+     * @param AuthorizationCheckerInterface $security
+     * @param FieldRepository               $repository
+     * @param EntityManagerInterface        $manager
+     */
+    public function __construct(AuthorizationCheckerInterface $security, FieldRepository $repository, EntityManagerInterface $manager)
+    {
+        $this->security   = $security;
+        $this->repository = $repository;
+        $this->manager    = $manager;
+    }
+
+    /**
+     * Command handler.
+     *
+     * @param SetFieldPositionCommand $command
+     *
+     * @throws AccessDeniedHttpException
+     * @throws NotFoundHttpException
+     */
+    public function handle(SetFieldPositionCommand $command)
+    {
+        /** @var \eTraxis\TemplatesDomain\Model\Entity\Field $field */
+        $field = $this->repository->find($command->field);
+
+        if (!$field || $field->isRemoved) {
+            throw new NotFoundHttpException();
+        }
+
+        if (!$this->security->isGranted(FieldVoter::UPDATE_FIELD, $field)) {
+            throw new AccessDeniedHttpException();
+        }
+
+        $fields = $field->state->fields;
+
+        $count = count($fields);
+
+        if ($command->position > $count) {
+            $command->position = $count;
+        }
+
+        $oldPosition = $field->position;
+
+        $this->setPosition($field, 0);
+
+        if ($oldPosition < $command->position) {
+            // Moving the field down.
+            for ($i = $oldPosition; $i < $command->position; $i++) {
+                $this->setPosition($fields[$i], $i);
+            }
+        }
+        elseif ($oldPosition > $command->position) {
+            // Moving the field up.
+            for ($i = $oldPosition; $i > $command->position; $i--) {
+                $this->setPosition($fields[$i - 2], $i);
+            }
+        }
+
+        $this->setPosition($field, $command->position);
+    }
+
+    /**
+     * Sets new position for specified field.
+     *
+     * @param Field $field
+     * @param int   $position
+     */
+    protected function setPosition(Field $field, int $position): void
+    {
+        $query = $this->manager->createQuery('
+            UPDATE TemplatesDomain:Field f
+            SET f.position = :position
+            WHERE f.id = :field
+        ');
+
+        $query->execute([
+            'field'    => $field->id,
+            'position' => $position,
+        ]);
+    }
+}
---
+++ tests/TemplatesDomain/Application/Command/Fields/SetOrderFieldCommandTest.php
@@ -0,0 +1,212 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\TemplatesDomain\Application\Command\Fields;
+
+use eTraxis\TemplatesDomain\Model\Entity\Field;
+use eTraxis\TemplatesDomain\Model\Entity\State;
+use eTraxis\Tests\TransactionalTestCase;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SetOrderFieldCommandTest extends TransactionalTestCase
+{
+    /** @var \eTraxis\TemplatesDomain\Model\Repository\FieldRepository */
+    protected $repository;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->repository = $this->doctrine->getRepository(Field::class);
+    }
+
+    public function testSuccessUp()
+    {
+        $expected = [
+            'Commit ID',
+            'Effort',
+            'Delta',
+            'Test coverage',
+        ];
+
+        $this->loginAs('admin@example.com');
+
+        /** @var Field $field */
+        [$field] = $this->repository->findBy(['name' => 'Effort'], ['id' => 'ASC']);
+
+        $command = new SetFieldPositionCommand([
+            'field'    => $field->id,
+            'position' => $field->position - 1,
+        ]);
+
+        $this->commandbus->handle($command);
+
+        self::assertSame($expected, $this->getFields($field->state));
+    }
+
+    public function testSuccessDown()
+    {
+        $expected = [
+            'Commit ID',
+            'Effort',
+            'Delta',
+            'Test coverage',
+        ];
+
+        $this->loginAs('admin@example.com');
+
+        /** @var Field $field */
+        [$field] = $this->repository->findBy(['name' => 'Delta'], ['id' => 'ASC']);
+
+        $command = new SetFieldPositionCommand([
+            'field'    => $field->id,
+            'position' => $field->position + 1,
+        ]);
+
+        $this->commandbus->handle($command);
+
+        self::assertSame($expected, $this->getFields($field->state));
+    }
+
+    public function testSuccessTop()
+    {
+        $expected = [
+            'Effort',
+            'Commit ID',
+            'Delta',
+            'Test coverage',
+        ];
+
+        $this->loginAs('admin@example.com');
+
+        /** @var Field $field */
+        [$field] = $this->repository->findBy(['name' => 'Effort'], ['id' => 'ASC']);
+
+        $command = new SetFieldPositionCommand([
+            'field'    => $field->id,
+            'position' => 1,
+        ]);
+
+        $this->commandbus->handle($command);
+
+        self::assertSame($expected, $this->getFields($field->state));
+    }
+
+    public function testSuccessBottom()
+    {
+        $expected = [
+            'Commit ID',
+            'Effort',
+            'Test coverage',
+            'Delta',
+        ];
+
+        $this->loginAs('admin@example.com');
+
+        /** @var Field $field */
+        [$field] = $this->repository->findBy(['name' => 'Delta'], ['id' => 'ASC']);
+
+        $command = new SetFieldPositionCommand([
+            'field'    => $field->id,
+            'position' => PHP_INT_MAX,
+        ]);
+
+        $this->commandbus->handle($command);
+
+        self::assertSame($expected, $this->getFields($field->state));
+    }
+
+    public function testAccessDenied()
+    {
+        $this->expectException(AccessDeniedHttpException::class);
+
+        $this->loginAs('artem@example.com');
+
+        /** @var Field $field */
+        [$field] = $this->repository->findBy(['name' => 'Effort'], ['id' => 'ASC']);
+
+        $command = new SetFieldPositionCommand([
+            'field'    => $field->id,
+            'position' => 1,
+        ]);
+
+        $this->commandbus->handle($command);
+    }
+
+    public function testUnlockedTemplate()
+    {
+        $this->expectException(AccessDeniedHttpException::class);
+
+        $this->loginAs('admin@example.com');
+
+        /** @var Field $field */
+        [$field] = $this->repository->findBy(['name' => 'Effort'], ['id' => 'DESC']);
+
+        $command = new SetFieldPositionCommand([
+            'field'    => $field->id,
+            'position' => 1,
+        ]);
+
+        $this->commandbus->handle($command);
+    }
+
+    public function testUnknownField()
+    {
+        $this->expectException(NotFoundHttpException::class);
+
+        $this->loginAs('admin@example.com');
+
+        $command = new SetFieldPositionCommand([
+            'field'    => self::UNKNOWN_ENTITY_ID,
+            'position' => 1,
+        ]);
+
+        $this->commandbus->handle($command);
+    }
+
+    public function testRemovedField()
+    {
+        $this->expectException(NotFoundHttpException::class);
+
+        $this->loginAs('admin@example.com');
+
+        /** @var Field $field */
+        [$field] = $this->repository->findBy(['name' => 'Task ID'], ['id' => 'ASC']);
+
+        $command = new SetFieldPositionCommand([
+            'field'    => $field->id,
+            'position' => 1,
+        ]);
+
+        $this->commandbus->handle($command);
+    }
+
+    /**
+     * @param State $state
+     *
+     * @return array
+     */
+    private function getFields(State $state)
+    {
+        /** @var Field[] $fields */
+        $fields = $this->repository->findBy([
+            'state'     => $state,
+            'removedAt' => null,
+        ], ['position' => 'ASC']);
+
+        return array_map(function (Field $field) {
+            return $field->name;
+        }, $fields);
+    }
+}
