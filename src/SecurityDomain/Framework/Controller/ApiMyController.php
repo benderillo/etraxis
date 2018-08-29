5d3852cbc36c8c7508077ebb83e053fa49944550
Artem Rodygin <artem.rodygin@gmail.com>
2018-08-29 18:38:51 +1200

    PATCH /api/my/profile

--- src/SecurityDomain/Framework/Controller/ApiMyController.php
+++ src/SecurityDomain/Framework/Controller/ApiMyController.php
@@ -13,10 +13,13 @@
 
 namespace eTraxis\SecurityDomain\Framework\Controller;
 
+use eTraxis\SecurityDomain\Application\Command\Users as Command;
 use eTraxis\SecurityDomain\Model\Dictionary\AccountProvider;
+use League\Tactician\CommandBus;
 use Swagger\Annotations as API;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -62,5 +65,51 @@ class ApiMyController extends Controller
             'fullname' => $user->fullname,
             'provider' => AccountProvider::get($user->account->provider),
         ]);
+    }
+
+    /**
+     * Updates profile of the current user.
+     *
+     * @Route("/profile", name="api_profile_update", methods={"PATCH"})
+     *
+     * @API\Parameter(name="email",    in="formData", type="string", required=false, description="Email address (RFC 5322).<br>Ignored for external accounts.")
+     * @API\Parameter(name="fullname", in="formData", type="string", required=false, description="Full name (up to 50 characters).<br>Ignored for external accounts.")
+     * @API\Parameter(name="locale",   in="formData", type="string", required=false, description="Locale ('xx' or 'xx_XX', see ISO 639-1 / ISO 3166-1).")
+     * @API\Parameter(name="theme",    in="formData", type="string", required=false, description="Theme.")
+     * @API\Parameter(name="timezone", in="formData", type="string", required=false, description="Timezone (IANA database value).")
+     *
+     * @API\Response(response=200, description="Success.")
+     * @Api\Response(response=400, description="The request is malformed.")
+     * @API\Response(response=401, description="Client is not authenticated.")
+     * @Api\Response(response=409, description="Account with specified email already exists.")
+     *
+     * @param Request    $request
+     * @param CommandBus $commandBus
+     *
+     * @return JsonResponse
+     */
+    public function updateProfile(Request $request, CommandBus $commandBus): JsonResponse
+    {
+        /** @var \eTraxis\SecurityDomain\Model\Entity\User $user */
+        $user = $this->getUser();
+
+        $profile = new Command\UpdateProfileCommand([
+            'email'    => $request->request->get('email', $user->email),
+            'fullname' => $request->request->get('fullname', $user->fullname),
+        ]);
+
+        $settings = new Command\UpdateSettingsCommand([
+            'locale'   => $request->request->get('locale', $user->locale),
+            'theme'    => $request->request->get('theme', $user->theme),
+            'timezone' => $request->request->get('timezone', $user->timezone),
+        ]);
+
+        if (!$user->isAccountExternal()) {
+            $commandBus->handle($profile);
+        }
+
+        $commandBus->handle($settings);
+
+        return $this->json(null);
     }
 }
---
+++ tests/SecurityDomain/Framework/Controller/ApiMyController/UpdateProfileTest.php
@@ -0,0 +1,141 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\SecurityDomain\Framework\Controller\ApiMyController;
+
+use eTraxis\SecurityDomain\Model\Entity\User;
+use eTraxis\Tests\TransactionalTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UpdateProfileTest extends TransactionalTestCase
+{
+    public function testSuccess()
+    {
+        /** @var User $user */
+        $user = $this->doctrine->getRepository(User::class)->findOneBy(['email' => 'nhills@example.com']);
+
+        self::assertSame('nhills@example.com', $user->email);
+        self::assertSame('Nikko Hills', $user->fullname);
+        self::assertSame('en_US', $user->locale);
+        self::assertSame('azure', $user->theme);
+        self::assertSame('UTC', $user->timezone);
+
+        $this->loginAs('nhills@example.com');
+
+        $uri = '/api/my/profile';
+
+        $data = [
+            'email'    => 'chaim.willms@example.com',
+            'fullname' => 'Chaim Willms',
+            'locale'   => 'es',
+            'theme'    => 'humanity',
+            'timezone' => 'Europe/Madrid',
+        ];
+
+        $response = $this->json(Request::METHOD_PATCH, $uri, $data);
+
+        self::assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $this->doctrine->getManager()->refresh($user);
+
+        self::assertSame('chaim.willms@example.com', $user->email);
+        self::assertSame('Chaim Willms', $user->fullname);
+        self::assertSame('es', $user->locale);
+        self::assertSame('humanity', $user->theme);
+        self::assertSame('Europe/Madrid', $user->timezone);
+    }
+
+    public function testSuccessPartial()
+    {
+        /** @var User $user */
+        $user = $this->doctrine->getRepository(User::class)->findOneBy(['email' => 'einstein@ldap.forumsys.com']);
+
+        self::assertSame('einstein@ldap.forumsys.com', $user->email);
+        self::assertSame('Albert Einstein', $user->fullname);
+        self::assertSame('en_US', $user->locale);
+        self::assertSame('azure', $user->theme);
+        self::assertSame('UTC', $user->timezone);
+
+        $this->loginAs('einstein@ldap.forumsys.com');
+
+        $uri = '/api/my/profile';
+
+        $data = [
+            'email'    => 'chaim.willms@example.com',
+            'fullname' => 'Chaim Willms',
+            'locale'   => 'de',
+            'theme'    => 'humanity',
+            'timezone' => 'Europe/Berlin',
+        ];
+
+        $response = $this->json(Request::METHOD_PATCH, $uri, $data);
+
+        self::assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $this->doctrine->getManager()->refresh($user);
+
+        self::assertSame('einstein@ldap.forumsys.com', $user->email);
+        self::assertSame('Albert Einstein', $user->fullname);
+        self::assertSame('de', $user->locale);
+        self::assertSame('humanity', $user->theme);
+        self::assertSame('Europe/Berlin', $user->timezone);
+    }
+
+    public function test400()
+    {
+        $this->loginAs('nhills@example.com');
+
+        $uri = '/api/my/profile';
+
+        $data = [
+            'email'    => 'invalid',
+            'fullname' => 'Chaim Willms',
+            'locale'   => 'es',
+            'theme'    => 'humanity',
+            'timezone' => 'Europe/Madrid',
+        ];
+
+        $response = $this->json(Request::METHOD_PATCH, $uri, $data);
+
+        self::assertSame(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+    }
+
+    public function test401()
+    {
+        $uri = '/api/my/profile';
+
+        $response = $this->json(Request::METHOD_PATCH, $uri);
+
+        self::assertSame(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function test409()
+    {
+        $this->loginAs('nhills@example.com');
+
+        $uri = '/api/my/profile';
+
+        $data = [
+            'email'    => 'ldoyle@example.com',
+            'fullname' => 'Chaim Willms',
+            'locale'   => 'es',
+            'theme'    => 'humanity',
+            'timezone' => 'Europe/Madrid',
+        ];
+
+        $response = $this->json(Request::METHOD_PATCH, $uri, $data);
+
+        self::assertSame(Response::HTTP_CONFLICT, $response->getStatusCode());
+    }
+}
