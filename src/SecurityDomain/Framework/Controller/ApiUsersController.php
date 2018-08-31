107bf88d6ebb3b3c83b4698d43bdda9a3a719cd7
Artem Rodygin <artem.rodygin@gmail.com>
2018-08-31 21:18:55 +1200

    POST /api/users/enable

--- src/SecurityDomain/Framework/Controller/ApiUsersController.php
+++ src/SecurityDomain/Framework/Controller/ApiUsersController.php
@@ -119,6 +119,32 @@ class ApiUsersController extends Controller
     }
 
     /**
+     * Enables specified users.
+     *
+     * @Route("/enable", name="api_users_enable", methods={"POST"})
+     *
+     * @API\Parameter(name="", in="body", @Model(type=Command\EnableUsersCommand::class, groups={"api"}))
+     *
+     * @API\Response(response=200, description="Success.")
+     * @API\Response(response=401, description="Client is not authenticated.")
+     * @API\Response(response=403, description="Client is not authorized for this request.")
+     * @API\Response(response=404, description="User is not found.")
+     *
+     * @param Request    $request
+     * @param CommandBus $commandBus
+     *
+     * @return JsonResponse
+     */
+    public function enableUsers(Request $request, CommandBus $commandBus): JsonResponse
+    {
+        $command = new Command\EnableUsersCommand($request->request->all());
+
+        $commandBus->handle($command);
+
+        return $this->json(null);
+    }
+
+    /**
      * Returns specified user.
      *
      * @Route("/{id}", name="api_users_get", methods={"GET"}, requirements={"id": "\d+"})
