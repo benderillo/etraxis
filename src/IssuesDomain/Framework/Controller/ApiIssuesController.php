fee64429b04e3261bff93bf7504c99e0225fbe83
Artem Rodygin <artem.rodygin@gmail.com>
2018-09-25 08:18:28 +1200

    GET /api/issues

--- src/IssuesDomain/Framework/Controller/ApiIssuesController.php
+++ src/IssuesDomain/Framework/Controller/ApiIssuesController.php
@@ -15,6 +15,7 @@ namespace eTraxis\IssuesDomain\Framework\Controller;
 
 use eTraxis\IssuesDomain\Application\Voter\IssueVoter;
 use eTraxis\IssuesDomain\Model\Entity\Issue;
+use eTraxis\IssuesDomain\Model\Repository\IssueRepository;
 use eTraxis\IssuesDomain\Model\Repository\LastReadRepository;
 use eTraxis\SharedDomain\Model\Collection\CollectionTrait;
 use Nelmio\ApiDocBundle\Annotation\Model;
@@ -22,6 +23,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Swagger\Annotations as API;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -35,6 +37,98 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiIssuesController extends Controller
 {
     use CollectionTrait;
+
+    /**
+     * Returns list of issues.
+     *
+     * @Route("", name="api_issues_list", methods={"GET"})
+     *
+     * @API\Parameter(name="offset",   in="query", type="integer", required=false, minimum=0, default=0, description="Zero-based index of the first issue to return.")
+     * @API\Parameter(name="limit",    in="query", type="integer", required=false, minimum=1, maximum=100, default=100, description="Maximum number of issues to return.")
+     * @API\Parameter(name="X-Search", in="body",  type="string",  required=false, description="Optional search value.", @API\Schema(type="string"))
+     * @API\Parameter(name="X-Filter", in="body",  type="object",  required=false, description="Optional filters.", @API\Schema(
+     *     type="object",
+     *     properties={
+     *         @API\Property(property="id",               type="string"),
+     *         @API\Property(property="subject",          type="string"),
+     *         @API\Property(property="author",           type="integer"),
+     *         @API\Property(property="author_name",      type="string"),
+     *         @API\Property(property="project",          type="integer"),
+     *         @API\Property(property="project_name",     type="string"),
+     *         @API\Property(property="template",         type="integer"),
+     *         @API\Property(property="template_name",    type="string"),
+     *         @API\Property(property="state",            type="integer"),
+     *         @API\Property(property="state_name",       type="string"),
+     *         @API\Property(property="responsible",      type="integer"),
+     *         @API\Property(property="responsible_name", type="string"),
+     *         @API\Property(property="is_cloned",        type="boolean"),
+     *         @API\Property(property="age",              type="integer"),
+     *         @API\Property(property="is_critical",      type="boolean"),
+     *         @API\Property(property="is_suspended",     type="boolean"),
+     *         @API\Property(property="is_closed",        type="boolean")
+     *     }
+     * ))
+     * @API\Parameter(name="X-Sort", in="body", type="object", required=false, description="Optional sorting.", @API\Schema(
+     *     type="object",
+     *     properties={
+     *         @API\Property(property="id",          type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="subject",     type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="created_at",  type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="changed_at",  type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="closed_at",   type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="author",      type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="project",     type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="template",    type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="state",       type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="responsible", type="string", enum={"ASC", "DESC"}, example="ASC"),
+     *         @API\Property(property="age",         type="string", enum={"ASC", "DESC"}, example="ASC")
+     *     }
+     * ))
+     *
+     * @API\Response(response=200, description="Success.", @API\Schema(
+     *     type="object",
+     *     properties={
+     *         @API\Property(property="from",  type="integer", example=0,   description="Zero-based index of the first returned issue."),
+     *         @API\Property(property="to",    type="integer", example=99,  description="Zero-based index of the last returned issue."),
+     *         @API\Property(property="total", type="integer", example=100, description="Total number of all found issues."),
+     *         @API\Property(property="data",  type="array", @API\Items(
+     *             ref=@Model(type=eTraxis\IssuesDomain\Model\API\Issue::class)
+     *         ))
+     *     }
+     * ))
+     * @API\Response(response=401, description="Client is not authenticated.")
+     *
+     * @param Request            $request
+     * @param IssueRepository    $repository
+     * @param LastReadRepository $lastReadRepository
+     *
+     * @return JsonResponse
+     */
+    public function listIssues(Request $request, IssueRepository $repository, LastReadRepository $lastReadRepository): JsonResponse
+    {
+        $collection = $this->getCollection($request, $repository);
+
+        /** @var \eTraxis\IssuesDomain\Model\Entity\LastRead[] $lastReads */
+        $lastReads = $lastReadRepository->findBy([
+            'issue' => $collection->data,
+            'user'  => $this->getUser(),
+        ]);
+
+        $values = [];
+
+        foreach ($lastReads as $lastRead) {
+            $values[$lastRead->issue->id] = $lastRead->readAt;
+        }
+
+        array_walk($collection->data, function (Issue &$issue) use ($values) {
+            $readAt = $values[$issue->id] ?? null;
+            $issue  = $issue->jsonSerialize();
+
+            $issue[Issue::JSON_READ_AT] = $readAt;
+        });
+
+        return $this->json($collection);
+    }
 
     /**
      * Returns specified issue.
---
+++ tests/IssuesDomain/Framework/Controller/ApiIssuesController/ListIssuesTest.php
@@ -0,0 +1,79 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\IssuesDomain\Framework\Controller\ApiIssuesController;
+
+use eTraxis\IssuesDomain\Model\Entity\Issue;
+use eTraxis\Tests\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ListIssuesTest extends WebTestCase
+{
+    public function testSuccess()
+    {
+        $this->loginAs('fdooley@example.com');
+
+        $expected = array_map(function (Issue $issue) {
+
+            $json = $issue->jsonSerialize();
+
+            switch ($issue->subject) {
+                case 'Development task 1':
+                    $json['read_at'] = $issue->closedAt;
+                    break;
+                case 'Development task 2':
+                    $json['read_at'] = $issue->createdAt;
+                    break;
+                case 'Development task 3':
+                    $json['read_at'] = $issue->closedAt;
+                    break;
+                case 'Development task 5':
+                    $json['read_at'] = $issue->createdAt;
+                    break;
+                case 'Development task 6':
+                    $json['read_at'] = $issue->createdAt;
+                    break;
+            }
+
+            return $json;
+        }, $this->doctrine->getRepository(Issue::class)->findBy([], ['id' => 'ASC']));
+
+        $uri = '/api/issues';
+
+        $response = $this->json(Request::METHOD_GET, $uri);
+
+        self::assertSame(Response::HTTP_OK, $response->getStatusCode());
+
+        $content = json_decode($response->getContent(), true);
+
+        self::assertSame(0, $content['from']);
+        self::assertSame(41, $content['to']);
+        self::assertSame(42, $content['total']);
+
+        usort($content['data'], function ($issue1, $issue2) {
+            return $issue1['id'] - $issue2['id'];
+        });
+
+        self::assertSame($expected, $content['data']);
+    }
+
+    public function test401()
+    {
+        $uri = '/api/issues';
+
+        $response = $this->json(Request::METHOD_GET, $uri);
+
+        self::assertSame(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+}
