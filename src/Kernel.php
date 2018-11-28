e171d4121c17efd0ebdfc9c1a7681df1027a31d0
Artem Rodygin <artem.rodygin@gmail.com>
2018-11-29 08:11:55 +1300

    Upgrade Symfony to 4.1.8

--- src/Kernel.php
+++ src/Kernel.php
@@ -72,7 +72,7 @@ class Kernel extends BaseKernel
         /** @noinspection PhpIncludeInspection */
         $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
-            if (isset($envs['all']) || isset($envs[$this->environment])) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
             }
         }
