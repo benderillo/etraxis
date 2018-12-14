6168193dbd79e71e6d8950e2a1812e1bf6fdd756
Artem Rodygin <artem.rodygin@gmail.com>
2018-12-14 17:55:52 +1300

    [skip ci] Modal dialogs

---
+++ assets/vue/modal.vue
@@ -0,0 +1,91 @@
+<template>
+    <dialog>
+        <form @submit.prevent>
+            <div class="modal-header">
+                <span class="pull-after fa fa-remove" :title="text.close" @click="onCancel"></span>
+                <div>{{ header }}</div>
+            </div>
+            <div class="modal-body">
+                <div class="grid-100">
+                    <div class="fieldset">
+                        <slot></slot>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer text-right">
+                <button type="submit" @click="onSubmit">{{ text.ok }}</button>
+                <button type="button" @click="onCancel">{{ text.cancel }}</button>
+            </div>
+        </form>
+    </dialog>
+</template>
+
+<script>
+
+    /**
+     * Modal dialog.
+     */
+    export default {
+
+        mounted() {
+
+            dialogPolyfill.registerDialog(this.$el);
+
+            this.$el.addEventListener('cancel', () => {
+                this.$emit('cancel');
+            });
+        },
+
+        props: {
+
+            /**
+             * Header text.
+             */
+            header: {
+                type: String,
+                required: true,
+            },
+        },
+
+        data: () => ({
+            text: {
+                close: i18n['button.close'],
+                ok: i18n['button.ok'],
+                cancel: i18n['button.cancel'],
+            },
+        }),
+
+        methods: {
+
+            /**
+             * Opens the dialog.
+             */
+            open() {
+                this.$el.showModal();
+            },
+
+            /**
+             * Closes the dialog.
+             */
+            close() {
+                this.$el.close();
+            },
+
+            /**
+             * Dialog is submitted (doesn't close automatically).
+             */
+            onSubmit() {
+                this.$emit('submit');
+            },
+
+            /**
+             * Dialog is cancelled (closes automatically).
+             */
+            onCancel() {
+                this.$el.close();
+                this.$emit('cancel');
+            },
+        },
+    };
+
+</script>
