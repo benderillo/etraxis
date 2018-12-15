e9c46ce9b2a87f4d29d8a3f4be2508289bc98f5b
Artem Rodygin <artem.rodygin@gmail.com>
2018-12-16 12:37:49 +1300

    [skip ci] Panels

---
+++ assets/vue/panel/list.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="panel-body">
+        <div v-for="item in items" :key="item.id" class="panel-item" :class="[ item.class, item.id === selected ? 'selected' : null ]">
+            <span class="fa fa-check"></span>
+            <a href="#" @click.prevent="$emit('click', item.id)">{{ item.title }}</a>
+        </div>
+        <div v-if="items.length === 0" class="panel-item empty">
+            <span class="fa fa-check"></span>
+            <a>&mdash;</a>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    /**
+     * Panel list.
+     */
+    export default {
+
+        props: {
+
+            /**
+             * Items are objects with the following keys: id, title, class (optional).
+             */
+            items: {
+                type: Array,
+                required: true,
+            },
+
+            /**
+             * ID of the currently selected item (can be null).
+             */
+            selected: {
+                required: false,
+                default: null,
+            },
+        },
+    };
+
+</script>
---
+++ assets/vue/panel/panel.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="panel" :class="{ 'collapsed': collapsed }">
+        <div class="panel-header" @click="collapsed = !collapsed">
+            <span v-if="!collapsed" class="pull-after fa fa-chevron-up" :title="text.collapse"></span>
+            <span v-if="collapsed" class="pull-after fa fa-chevron-down" :title="text.expand"></span>
+            <span class="pull-after fa fa-plus" :title="text.create" @click.stop="$emit('create')"></span>
+            <div>{{ header }}</div>
+        </div>
+        <div v-show="!collapsed">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    /**
+     * Panel.
+     */
+    export default {
+
+        props: {
+
+            /**
+             * Panel's header text.
+             */
+            header: {
+                type: String,
+                required: true,
+            },
+        },
+
+        data: () => ({
+            collapsed: false,
+            text: {
+                create: i18n['button.create'],
+                collapse: i18n['button.collapse'],
+                expand: i18n['button.expand'],
+            },
+        }),
+    };
+
+</script>
